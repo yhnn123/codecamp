720ebe7239f96f316db670d6eb48cf663f991c23
yhnn123 <yhnn123@gmail.com>
2015-10-31 15:58:34 +0900

    my codecademy_section_01

---
+++ codecademy_python_section_01.py
@@ -0,0 +1,257 @@
+# 1
+
+print "Welcom to Python!"
+print "hi"
+
+
+
+# 2
+
+my_variable = 10
+
+print my_variable
+
+
+
+# 3
+
+# 앞서 설명 란에서 얘기한 변수와 각 변수의 값들을 지정하세요!
+
+my_int = 7
+my_float = 1.23
+my_bool = True
+
+print my_int
+print my_float
+print my_bool
+
+
+
+# 4
+
+# 아래에 변수 my_int의 값은 7로 지정되어 있습니다.
+# 지정된 값을 3으로 변경하고 결과를 출력하려면 어떻게 해야할까요?
+
+my_int = 7
+
+# 여덟 번째 줄에서 변수 my_int의 값을 3 으로 변경하세요!
+
+my_int = 3
+
+# 변수 my_int의 값을 console에 출력하는 코드는 아래와 같습니다:
+# 여기서 사용된 print 키워드는 이후 더 자세히 다룰 예정입니다!
+
+print my_int
+
+
+
+# 5
+
+def spam():
+eggs = 12
+return eggs
+        
+print spam()
+
+
+
+# 6
+
+def spam():
+    eggs = 12
+    return eggs
+        
+print spam()
+
+
+
+# 7
+
+spam = True
+eggs = False
+
+
+
+# 8 
+
+mysterious_variable = 42 # 주석이당
+
+
+
+# 9
+
+""" 주석이당
+주석이당
+주석이당
+주
+석
+주
+석 """
+
+
+
+# 10
+
+# 세 번째 줄에서 변수 count_to의 값으로 1 더하기 2를 지정하세요!
+
+count_to = 1+2
+
+print count_to
+
+
+
+# 11
+
+# 세 번째 줄에서 거듭제곱을 이용하여 변수 eggs의 값을 100으로 지정하세요!
+
+eggs = 10 ** 2
+
+print eggs
+
+
+
+# 12
+
+# 세 번째 줄에서 모듈로 연산을 이용하여 변수 spam의 값을 1로 지정하세요!
+
+spam = 5 % 2
+
+print spam
+
+
+
+# 13
+
+# 주석을 작성하래 ㅋㅋ
+
+monty = True
+python = 1.234
+monty_python = python ** 2
+
+print monty
+print python
+print monty_python
+
+
+
+# 14
+
+# 세 번째 줄에 변수 variable의 값으로 44.50을 지정하세요!
+
+meal = 44.50 
+
+
+
+# 15
+
+meal = 44.50
+
+tax = 0.0675
+
+
+
+# 16
+
+# 거의 다 됐습니다! 다섯 번째 줄에 변수 tip을 생성하세요.
+
+meal = 44.50
+tax = 0.0675
+tip = 0.15
+
+
+
+# 17
+
+# Reassign meal on line 7!
+
+meal = 44.50
+tax = 0.0675
+tip = 0.15
+
+meal = meal + (meal * tax)
+
+
+
+# 18
+
+# 여덟 번째 줄에 변수 total을 생성하세요!
+
+meal = 44.50
+tax = 0.0675
+tip = 0.15
+
+meal = meal + meal * tax
+total = meal + meal * tip
+
+print("%.2f" % total)
+
+
+
+# 19
+
+# 세 번째 줄에 변수 brain을 생성하세요!
+
+brian = "언제나 삶의 긍정적인 면을 살피세요!"
+
+
+
+# 20
+
+# 각 줄마다 하나씩, 변수들을 생성하고 값을 지정하세요!
+
+caesar = "Graham"
+praline = "John"
+viking = "Teresa"
+
+# 변수들은 이 줄 위에 작성되어야 합니다.
+
+print caesar
+print praline
+print viking
+
+
+# 21
+
+# 아래의 문자열에 문제가 있습니다. 백슬래쉬를 이용하여 고쳐보세요!
+
+'Help! Help! I\'m being repressed!'
+
+
+
+# 22
+
+"""
+문자열 "PYTHON"은 여섯개의 문자를 가지고 있고,
+아래와 같이 0부터 5까지 숫자가 매겨져 있습니다:
+
++---+---+---+---+---+---+
+| P | Y | T | H | O | N |
++---+---+---+---+---+---+
+  0   1   2   3   4   5
+
+따라서 여러분이 "Y"를 원한다면, 다음과 같이 작성하면 됩니다:
+"PYTHON"[1] (언제나 순서는 0부터 세기 시작합니다!)
+"""
+fifth_letter = "MONTY"[4]
+
+print fifth_letter
+
+
+
+# 23
+
+parrot = "Norwegian Blue"
+print len(parrot)
+
+
+
+# 24
+
+parrot = "Norwegian Blue"
+
+print parrot.lower()
+
+
+
+# 25
+
